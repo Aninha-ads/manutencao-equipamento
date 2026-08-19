bd8e6b62e9c376bf89d949076cfedeea2d5e0600
Ana-Clara <ana.batista@ufpr.br>
2026-08-19 19:06:04 -0300

    feat: componente de autocadastro + viacep

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+import { Routes } from '@angular/router';
+import { CadastroClienteComponent } from './features/cadastro-cliente/cadastro-cliente.component'; 
+export const routes: Routes = [
+  { path: 'cadastro', component: CadastroClienteComponent },
+ 
+];
