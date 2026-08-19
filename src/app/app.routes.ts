9fa7e9a4421d50dd3477fae6b2dede0182ef6437
Nilmar <nilmar81@gmail.com>
2026-08-19 20:53:26 -0300

    Criei o prototipo do painel do cliente RF003

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PainelClienteComponent } from './pages/painel-cliente/painel-cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'painel-cliente',
+        component: PainelClienteComponent
+    }
+];
