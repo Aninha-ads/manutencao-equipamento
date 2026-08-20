5e8721c59237427b1e6730834080e123ffb4901c
Nilmar Pierin <120408603+nilmar81@users.noreply.github.com>
2026-08-20 16:03:33 -0300

    Add route for CadastroClienteComponent
    Correção de conflito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,14 @@
 import { Routes } from '@angular/router';
 import { PainelClienteComponent } from './pages/painel-cliente/painel-cliente.component';
+import { CadastroClienteComponent } from './features/cadastro-cliente/cadastro-cliente.component';
 
 export const routes: Routes = [
-    {
-        path: 'painel-cliente',
-        component: PainelClienteComponent
-    }
-];
-
-
-
-import { Routes } from '@angular/router';
-import { CadastroClienteComponent } from './features/cadastro-cliente/cadastro-cliente.component'; 
-export const routes: Routes = [
-  { path: 'cadastro', component: CadastroClienteComponent },
- 
+  {
+    path: 'painel-cliente',
+    component: PainelClienteComponent
+  },
+  {
+    path: 'cadastro',
+    component: CadastroClienteComponent
+  }
 ];
