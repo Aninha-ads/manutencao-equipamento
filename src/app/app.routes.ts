3dcb6f26d7402496eb141f531edbb1dbe519b265
Clarissa Pietrangelo <clarissapietrangelo@gmail.com>
2026-08-25 12:36:08 -0300

    Fix. App routes - arrumando as rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
+
 import { PainelClienteComponent } from './pages/painel-cliente/painel-cliente.component';
 import { CadastroClienteComponent } from './features/cadastro-cliente/cadastro-cliente.component';
+import { LoginComponent } from './pages/login/login';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
   {
     path: 'painel-cliente',
     component: PainelClienteComponent
