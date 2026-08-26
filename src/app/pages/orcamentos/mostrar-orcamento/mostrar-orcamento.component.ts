81be445e208541d262bc4fa5b64f68730cf3bdaf
Nilmar <nilmar81@gmail.com>
2026-08-26 12:24:02 -0300

    Estrutura Orçamentos RF012 e RF005

---
+++ src/app/pages/orcamentos/mostrar-orcamento/mostrar-orcamento.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-orcamento',
+  imports: [],
+  templateUrl: './mostrar-orcamento.component.html',
+  styleUrl: './mostrar-orcamento.component.css',
+})
+export class MostrarOrcamentoComponent {}
