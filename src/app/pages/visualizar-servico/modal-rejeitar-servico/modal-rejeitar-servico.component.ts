52b439165d1bcd6816d9c51d9a5af592a39a97bf
Ana Clara <anaclarajarbascotta@gmail.com>
2026-09-02 00:13:00 -0300

    alterações do model e resolvendo bugs

--- src/app/pages/visualizar-servico/modal-rejeitar-servico/modal-rejeitar-servico.component.ts
+++ src/app/pages/visualizar-servico/modal-rejeitar-servico/modal-rejeitar-servico.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-import{ NgbActioveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-modal-rejeitar-servico',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './modal-rejeitar-servico.component.html',
   styleUrl: './modal-rejeitar-servico.component.css',
 })
-export class ModalRejeitarServicoComponent {}
+export class ModalRejeitarServicoComponent {
+  activeModal: NgbActiveModal = inject(NgbActiveModal);
+  motivoRejeicao = '';
+
+  confirmarRejeicao() {
+    this.activeModal.close(this.motivoRejeicao);
+  }
+}
