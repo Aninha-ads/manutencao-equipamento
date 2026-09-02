fd1e312eb485fc8e7344d975393f49941a133e8b
Ana Clara <anaclarajarbascotta@gmail.com>
2026-09-01 22:57:30 -0300

    modal rejeicao de servico

---
+++ src/app/pages/visualizar-servico/modal-rejeitar-servico/modal-rejeitar-servico.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import{ NgbActioveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal-rejeitar-servico',
+  imports: [],
+  templateUrl: './modal-rejeitar-servico.component.html',
+  styleUrl: './modal-rejeitar-servico.component.css',
+})
+export class ModalRejeitarServicoComponent {}
