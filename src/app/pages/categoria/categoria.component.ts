739facc4712773dec7d2604e3d62f42e636314a1
Ana-Clara <ana.batista@ufpr.br>
2026-09-05 21:02:39 -0300

    feat: formulário de categoria de equipamento

---
+++ src/app/pages/categoria/categoria.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categoria',
+  imports: [],
+  templateUrl: './categoria.component.html',
+  styleUrl: './categoria.component.css',
+})
+export class CategoriaComponent {}
