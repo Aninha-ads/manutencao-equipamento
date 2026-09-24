1260feab57dc0defabbaeb9313451aace40a9fc5
Ana Clara <anaclarajarbascotta@gmail.com>
2026-09-24 12:14:55 -0300

    adiciona modelos e servicos de cliente e funcionario com json-server

--- Front/src/app/app.ts
+++ Front/src/app/app.ts
@@ -1,9 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { Router,RouterOutlet } from '@angular/router';
+import { Component, signal, inject, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 //seguindo o slide(279) do professor
 //decidi tirar o NavbarComponent pois ele não vai ser mais utilizado
 /*import { NavbarComponent } from './pages/navbar/navbar.component';*/
 import { FooterComponent } from './pages/footer/footer.component';
+import { ClienteService } from './services/cliente.service';
+import { Cliente } from './models/cliente.model';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,16 @@ import { FooterComponent } from './pages/footer/footer.component';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('manutencao-equipamento');
-    constructor(public router: Router) {}
+
+  private clienteService = inject(ClienteService);
+
+  constructor(public router: Router) {}
+
+  ngOnInit() {
+    this.clienteService.listarTodos().subscribe({
+      next: (data: Cliente[]) => console.log(data)
+    });
+  }
 }
